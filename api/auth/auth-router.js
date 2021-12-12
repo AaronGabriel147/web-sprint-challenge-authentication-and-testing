a71d5fd9d3781cf58fcb169d1414db12b9b9716c
AaronGabriel147 <77466761+AaronGabriel147@users.noreply.github.com>
2021-12-11 16:18:12 -0800

    mostly set up, imports, tokens, and bcrypt

--- api/auth/auth-router.js
+++ api/auth/auth-router.js
@@ -1,6 +1,8 @@
 const router = require('express').Router();
 const Users = require('./auth-model')
-
+// const bcrypt = require('bcryptjs')
+// const jwt = require('jsonwebtoken')
+// const { JWT_SECRET, NUM } = require('../../config/secrets') // I think????
 
 
 
@@ -54,7 +56,6 @@ router.post('/register', async (req, res, next) => {
   // console.log('reg')
   try {
     const newUser = await Users.create(req.body); // req.body is the object that was sent in the request
-
     if (!newUser) {
       res.status(404).json({
         message: "The post with the specified ID does not exist"
@@ -69,32 +70,32 @@ router.post('/register', async (req, res, next) => {
 
 
 
+/*
+IMPLEMENT
+You are welcome to build additional middleware to help with the endpoint's functionality.
+
+1- In order to log into an existing account the client must provide `username` and `password`:
+{
+  "username": "Captain Marvel",
+  "password": "foobar"
+}
+
+2- On SUCCESSFUL login,
+the response body should have `message` and `token`:
+{
+  "message": "welcome, Captain Marvel",
+  "token": "eyJhbGciOiJIUzI ... ETC ... vUPjZYDSa46Nwz8"
+}
+
+3- On FAILED login due to `username` or `password` missing from the request body,
+the response body should include a string exactly as follows: "username and password required".
+
+4- On FAILED login due to `username` not existing in the db, or `password` being incorrect,
+the response body should include a string exactly as follows: "invalid credentials".
+*/
 
 router.post('/login', (req, res) => {
   res.end('implement login, please!');
-  /*
-    IMPLEMENT
-    You are welcome to build additional middleware to help with the endpoint's functionality.
-
-    1- In order to log into an existing account the client must provide `username` and `password`:
-      {
-        "username": "Captain Marvel",
-        "password": "foobar"
-      }
-
-    2- On SUCCESSFUL login,
-      the response body should have `message` and `token`:
-      {
-        "message": "welcome, Captain Marvel",
-        "token": "eyJhbGciOiJIUzI ... ETC ... vUPjZYDSa46Nwz8"
-      }
-
-    3- On FAILED login due to `username` or `password` missing from the request body,
-      the response body should include a string exactly as follows: "username and password required".
-
-    4- On FAILED login due to `username` not existing in the db, or `password` being incorrect,
-      the response body should include a string exactly as follows: "invalid credentials".
-  */
 });
 
 module.exports = router;
--- api/server.js
+++ api/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const helmet = require('helmet');
+const session = require('express-session');
 
 const restrict = require('./middleware/restricted.js');
 
@@ -16,4 +17,19 @@ server.use(express.json());
 server.use('/api/auth', authRouter);
 server.use('/api/jokes', restrict, jokesRouter); // only logged-in users should have access!
 
+
+server.use(errorHandling) // will trap "".catch/500 errors" happening above
+
 module.exports = server;
+
+
+
+
+// *catch all 500 errors middleware* 
+function errorHandling(err, req, res, next) {
+    console.log('@@@***inside catch all 500***@@@'),
+        res.status(err.status || 500).json({
+            message: err.message,
+            status: 500
+        })
+}
